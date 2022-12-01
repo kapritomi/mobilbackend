16e3833e3e400c0eab9c8ea879873f758250fb83
kapritomi <kapritomi@gmail.com>
2022-12-01 19:03:31 +0100

    ja

--- backend.js
+++ backend.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const mysql = require('mysql')
 const app = express()
-const port = 3000
+const port = 24003
 const cors = require('cors')
 var connection
 
 function kapcsolat() {
     connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'bevasarlolista'
+        host: '192.168.0.200',
+        user: 'u61_8weC7VZ9A0',
+        password: 'gfkdWtuh+Ai5V=hUL^ZUJ3Ug',
+        database: 's61_db'
     })
 }
 
